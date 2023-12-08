2d51dfe2b9d25a3d70081b24d7a86b3a92d3b036
edinvucinic <edinvucinic@gmail.com>
2023-12-08 12:24:41 +0100

    addingJavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  
