46016a4366cb8e440d7569ee989227773d607fee
edinvucinic@gmail.com <edinvucinic@gmail.com>
2024-01-11 19:03:51 +0100

    addingFirstStructureOfReactApp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./style.css";
 
 function App() {
+  const appTitle = "Fun Facts App";
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Fun Facts App</h1>
-      </div>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="68" width="68" alt="Fun Facts App Logo" />
+          <h1>{appTitle}</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter /> {/* Corrected the function name */}
+        <FactList />
+      </main>
+    </>
   );
 }
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  /* Corrected the function name */
+  return <aside>Category filter</aside>;
+}
+function FactList() {
+  return <section>Facts list</section>;
+}
+
 export default App;
